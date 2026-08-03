a6c2750af7af853d2c07d63b57ee36673d03f5db
Dũng Hehe <ducdung110206@gmail.com>
2026-08-03 07:56:43 +0700

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -1,50 +1,21 @@
-package Chuong5_Bai2;
+package Chuong5_Bai3;
 
-public class Rectangle extends Shape {
+public class Rectangle implements Polygon {
+     private double length;
     private double width;
-    private double length;
-    public Rectangle(){
-        super();
-        this.width=1.0;
-        this.length=1.0;
-    }
-    public Rectangle(double width,double length){
-        super();
-        this.width=width;
+    public Rectangle(double length ,double width){
         this.length=length;
-        
-    }
-    public Rectangle(double width,double length,String color,boolean filled){
-        super(color,filled);
-        this.width=width;
-        this.length=length;
-        
-    }
-    @Override
-    public double getArea() {
-        return width*length;
-    }
-    @Override
-    public double getPerimeter() {
-        return 2*(width+length);
-    }
-    @Override
-    public String toString() {
-        return "Rectangle[width=" + width + ", length=" + length + 
-               ", color=" + getColor() + ", filled=" + isFilled() + "]";
-    }
-    public double getWidth(){
-        return width;
-    }
-    public void setWidth(double width){
         this.width=width;
     }
-    public double getLength(){
-        return length;
+    //Ghi de phuong thuc area:  S = dai * rong
+    @Override
+    public double area(){
+        return (length*width);
     }
-    public void setLength(double length){
-        this.length=length;
-    }
-
+    // Getter cho height và base theo sơ đồ
+    public double getLength(){return length;}
+    public void setLength(double length){this.length=length;}
+    public double getWidth(){return width;}
+    public void setWidth(double width){this.width=width;}
     
 }
